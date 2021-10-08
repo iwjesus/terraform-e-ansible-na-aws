d68c4bedc32a8f5082251c14dffec74ef08e8352
Irlani Jesus <irlani.wilson@gmail.com>
2021-10-08 09:21:40 -0300

    primeiro commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "sa-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-054a31f1b3bf90920"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+
+  tags = {
+    Name = "primeirainstancia"
+  }
+}
