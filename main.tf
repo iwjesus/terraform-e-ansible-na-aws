628d26cabbc624d29c062c5916809ad603f256b6
iwjesus <irlani.wilson@gmail.com>
2021-10-14 21:31:16 -0300

    configuracao playbook ansible

--- main.tf
+++ main.tf
@@ -18,29 +18,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-054a31f1b3bf90920"
   instance_type = "t2.micro"
   key_name = "iac-alura"
-  user_data = <<-EOF
-                #!/bin/bash
-                cd /home/ubuntu
-<<<<<<< HEAD
-<<<<<<< HEAD
-                echo “<h1>feito com terraform</h1>” > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
-
-                 nohup busybox httpd -f -p 8080 &
-                EOF
-=======
-                echo “<h1>Só o Santos parou a guerra!</h1>” > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
->>>>>>> 35ec581eef656c924bb8676c2bee463c8024d919
-=======
-                echo “<h1>Só o Santos parou a guerra!</h1>” > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
->>>>>>> 35ec581eef656c924bb8676c2bee463c8024d919
-
+  #user_data = <<-EOF
+  #              #!/bin/bash
+  #              cd /home/ubuntu
+  #              echo “<h1>Só Santos parou a Guerra!!</h1>” > index.html
+  #              nohup busybox httpd -f -p 8080 &
+  #              EOF
   tags = {
-    Name = "teste aws"
+    Name = "segunda instancia"
   }
 }
